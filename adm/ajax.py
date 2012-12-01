9c9372513baefea416f6f2b96d991eaee2b4a427
RK <romainknezevic@gmail.com>
2012-12-01 18:50:40 +0100

    dajaxImprove -- onload

--- adm/ajax.py
+++ adm/ajax.py
@@ -5,17 +5,10 @@ from dajax.core import Dajax
 from models import PageBook, Book
 
 def getImage(request, selector, book, page, bEven):
-#    dajax = Dajax()
-
     if bool(bEven):
         page = Book.objects.all()[int(book)].pagebooks.all()[int(page)].pageGauche.url
     else:
         page = Book.objects.all()[int(book)].pagebooks.all()[int(page)].pageDroite.url
-
-#    tag = "<img class=\"pageBook\" alt=\"Photo book\" src=\"" + page + "\" />"
-
-#    dajax.remove('#' + selector + ' .loader')
-#    dajax.append('#' + selector, 'innerHTML', tag)
 
     dajax = Dajax()
     dajax.script('ajaxOnLoad("'+selector+'", "'+page+'");')
